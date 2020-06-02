a0e5fcc87d503636e1feebd7e7898bdbdf2b3eb3
mdnmnahmed <mdnmnahmed@gmail.com>
2020-06-02 06:17:00 +0530

    Updates

---
+++ precache-manifest.86afbdfba70b34e61de44ca0c260cb73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101bfe8785a5a1d3d8d51fdf448862d5",
+    "url": "/ToDo-App-React/index.html"
+  },
+  {
+    "revision": "824c34ea7c4b4b99a019",
+    "url": "/ToDo-App-React/static/css/main.0acd4e68.chunk.css"
+  },
+  {
+    "revision": "067e2d63360ad0295d04",
+    "url": "/ToDo-App-React/static/js/2.c6cc88f5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ToDo-App-React/static/js/2.c6cc88f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824c34ea7c4b4b99a019",
+    "url": "/ToDo-App-React/static/js/main.a51a4cc0.chunk.js"
+  },
+  {
+    "revision": "2a2bb27a33cfce1da3ae",
+    "url": "/ToDo-App-React/static/js/runtime-main.2f090390.js"
+  }
+]);
